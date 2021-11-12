573b00539c275b194d5ba32ef2d9c35422c7d01a
Máté Turner <mate.turner@stud.uni-corvinus.hu>
2021-11-12 17:45:03 +0100

    abszfut

--- 8gyak/8gyak/Entities/CarFactory.cs
+++ 8gyak/8gyak/Entities/CarFactory.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace _8gyak.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
