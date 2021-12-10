af8ab388b49e6356741dc5538cf1b9bb3fe6b186
Máté Turner <mate.turner@stud.uni-corvinus.hu>
2021-12-10 22:15:40 +0100

    2.

--- 12gyak/12gyak/Form1.cs
+++ 12gyak/12gyak/Form1.cs
@@ -15,14 +15,21 @@ namespace _12gyak
     {
         GameController gc = new GameController();
         GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
-            gc.AddPlayer();
+            for (int i = 0; i <populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
             gc.Start(true);
-
         }
     }
 }
